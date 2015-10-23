5a3f798f87d9a383fb15367016494c614787ec2e
AutumnLight <zhenxi632965668@gmail.com>
2015-10-23 10:26:18 +0800

    修改了NormalizedDistance中dist变量为引用类型，并使用装饰者模式对该类进行了重新设计。

--- algorithm/algorithm/distance.h
+++ algorithm/algorithm/distance.h
@@ -17,51 +17,17 @@ namespace ml { // ml为machine learning的缩写
 	{
 	public:
 		typedef typename Container::size_type size_t;
-		double operator() (Container &c1, Container &c2)
-		{
-			size_t len1 = c1.size();
-			size_t len2 = c2.size();
-			size_t maxLen = len1 > len2 ? len1 : len2;
-
-			int **distanceMatrix = new int*[len1 + 1];
-
-			size_t i, j;
-			for (i = 0; i <= len1; ++i)
-				distanceMatrix[i] = new int[len2 + 1];
-
-			// 第一行-->初始化为0
-			for (i = 0; i <= len2; ++i) distanceMatrix[0][i] = 0;
-
-			// 第一列-->初始化为0
-			for (i = 1; i < len1; ++i) distanceMatrix[i][0] = 0;
-
-			for (i = 1; i <= len1; ++i)
-				for (j = 1; j <= len2; ++j)
-					if (c1[i] == c2[j])
-						distanceMatrix[i][j] = distanceMatrix[i - 1][j - 1];
-					else
-						distanceMatrix[i][j] = 1 +
-						min(distanceMatrix[i][j - 1],		// 插入
-							min(distanceMatrix[i - 1][j],		// 删除
-								distanceMatrix[i - 1][j - 1])); // 修改
-
-			double dist = distanceMatrix[len1][len2];
-
-			for (i = 0; i < len1; ++i)
-				delete[] distanceMatrix[i];
-
-			delete[] distanceMatrix;
-			return dist;
-		}
-
+		double operator() (Container &c1, Container &c2);
 	};
 
-	// 标准化后的距离
-	template <typename Distance>
-	class NormalizedDistance {
+	// 标准化后的距离，参见weka标准化距离度量
+	// 这里使用装饰着模式进行设计
+	template <typename Container>
+	class NormalizedDistance : public Distance<Container>
+	{
 	public:
-		typedef typename Distance::Container Container;
 		typedef typename Container::size_type size_t;
+		NormalizedDistance(Distance<Container> &d) : dist(d) {}
 		double operator() (Container &c1, Container &c2)
 		{
 			double maxLen = c1.size() > c2.size() ? c1.size() : c2.size();
@@ -69,8 +35,48 @@ namespace ml { // ml为machine learning的缩写
 			return d / maxLen;
 		}
 	private:
-		Distance dist;
+		Distance &dist; // 这里应该声明为引用，否则会报“无法实例化抽象类”
+						// 引用类型必须在构造函数初始化列表中初始化，常量同样
 	};
+
+	// EditDistance的实现
+	template <typename Container>
+	double EditDistance<Container>::operator() (Container &c1, Container &c2)
+	{
+		size_t len1 = c1.size();
+		size_t len2 = c2.size();
+		size_t maxLen = len1 > len2 ? len1 : len2;
+
+		int **distanceMatrix = new int*[len1 + 1];
+
+		size_t i, j;
+		for (i = 0; i <= len1; ++i)
+			distanceMatrix[i] = new int[len2 + 1];
+
+		// 第一行-->初始化为0
+		for (i = 0; i <= len2; ++i) distanceMatrix[0][i] = 0;
+
+		// 第一列-->初始化为0
+		for (i = 1; i < len1; ++i) distanceMatrix[i][0] = 0;
+
+		for (i = 1; i <= len1; ++i)
+			for (j = 1; j <= len2; ++j)
+				if (c1[i] == c2[j])
+					distanceMatrix[i][j] = distanceMatrix[i - 1][j - 1];
+				else
+					distanceMatrix[i][j] = 1 +
+					min(distanceMatrix[i][j - 1],		// 插入
+						min(distanceMatrix[i - 1][j],		// 删除
+							distanceMatrix[i - 1][j - 1])); // 修改
+
+		double dist = distanceMatrix[len1][len2];
+
+		for (i = 0; i < len1; ++i)
+			delete[] distanceMatrix[i];
+
+		delete[] distanceMatrix;
+		return dist;
+	}
 }
 
 #endif
