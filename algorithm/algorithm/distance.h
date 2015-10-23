80f6b96825ae4cba4fea15f5720011620b04144f
AutumnLight <zhenxi632965668@gmail.com>
2015-10-23 17:38:09 +0800

    修复EditDistance字符比较下标错误的bug

--- algorithm/algorithm/distance.h
+++ algorithm/algorithm/distance.h
@@ -9,7 +9,7 @@ namespace ml { // ml为machine learning的缩写
 	{
 	public:
 		typedef T Container;
-		virtual double operator() (T &t1, T &t2) = 0;
+		virtual double operator() (const T &t1, const T &t2) = 0;
 	};
 
 	template <typename Container>
@@ -17,7 +17,7 @@ namespace ml { // ml为machine learning的缩写
 	{
 	public:
 		typedef typename Container::size_type size_t;
-		double operator() (Container &c1, Container &c2);
+		double operator() (const Container &c1, const Container &c2);
 	};
 
 	// 标准化后的距离，参见weka标准化距离度量
@@ -28,7 +28,7 @@ namespace ml { // ml为machine learning的缩写
 	public:
 		typedef typename Container::size_type size_t;
 		NormalizedDistance(Distance<Container> &d) : dist(d) {}
-		double operator() (Container &c1, Container &c2)
+		double operator() (const Container &c1, const Container &c2)
 		{
 			double maxLen = c1.size() > c2.size() ? c1.size() : c2.size();
 			double d = dist(c1, c2);
@@ -41,7 +41,7 @@ namespace ml { // ml为machine learning的缩写
 
 	// EditDistance的实现
 	template <typename Container>
-	double EditDistance<Container>::operator() (Container &c1, Container &c2)
+	double EditDistance<Container>::operator() (const Container &c1, const Container &c2)
 	{
 		size_t len1 = c1.size();
 		size_t len2 = c2.size();
@@ -61,7 +61,7 @@ namespace ml { // ml为machine learning的缩写
 
 		for (i = 1; i <= len1; ++i)
 			for (j = 1; j <= len2; ++j)
-				if (c1[i] == c2[j])
+				if (c1[i-1] == c2[j-1])
 					distanceMatrix[i][j] = distanceMatrix[i - 1][j - 1];
 				else
 					distanceMatrix[i][j] = 1 +
