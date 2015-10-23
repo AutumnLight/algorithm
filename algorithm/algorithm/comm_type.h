11590ddc313b79215879ebe8079a4610465c4bea
AutumnLight <zhenxi632965668@gmail.com>
2015-10-23 17:38:47 +0800

    常用的类型定义

---
+++ algorithm/algorithm/comm_type.h
@@ -0,0 +1,23 @@
+/*************************************************************************
+	> File Name: commdef.h
+	> Author: zl
+	> Mail: zhaolei123@126.com 
+	> Created Time: Tue 11 Aug 2015 03:46:21 PM CST
+ ************************************************************************/
+
+// 记录一些经常使用的公共类型
+#ifndef COMMON_TYPE_H
+#define COMMON_TYPE_H
+
+#include <vector>
+using namespace std;
+
+typedef vector<string> vs;
+typedef vector<int> vint;
+typedef vector<bool> vb;
+typedef vector<vector<string>> vvs;
+typedef vector<vector<int>> vvint;
+typedef vector<double> vdouble;
+typedef vector<vector<double>> vvd;
+
+#endif
