59ad3072cd364d69a5009917fb8d78967550bdfa
AutumnLight <zhenxi632965668@gmail.com>
2015-10-28 21:30:12 +0800

    更改解决方案名称

--- algorithm/algorithm/quick_sort.cpp
+++ algorithm/algorithm/quick_sort.cpp
@@ -1,6 +1,10 @@
 #include "stdafx.h"
-#include "partition.h"
 #include <iostream>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include "partition.h"
+
 namespace autumnlight {
 
 // quick sort 递归实现版本
@@ -43,6 +47,69 @@ void quick_sort_nonrecursion(T *arr, size_t len)
 	}
 }
 
+// 插入排序-->时间复杂度在最好的情况下可以达到O(n)
+template<typename T>
+void insert_sort(T *arr, int len)
+{
+	if (len <= 0 || !arr) return;
+	for (int i = 1; i < len; ++i)
+	{
+		T tmp(arr[i]);
+		int j;
+		for (j = i - 1; j >= 0 && arr[j] > tmp; --j)
+			arr[j + 1] = arr[j];
+		arr[j + 1] = tmp;
+	}
+}
+
+// 普通的冒泡排序
+template<typename T>
+void bubble_sort(T *arr, int len)
+{
+	if (!arr || len <= 0) return;
+
+	int i, j;
+	for (i = 1; i < len; ++i) // len-1趟
+	{
+		for (j = 0; j < len - i; ++j)
+			if (arr[j] > arr[j + 1])
+				swap(arr[j], arr[j + 1]);
+	}
+}
+
+// 改进的冒泡排序-->时间复杂度在最好情况下可以达到O(n)
+template<typename T>
+void ibubble_sort(T *arr, int len)
+{
+	if (!arr || len <= 0) return;
+	
+	int i, j;
+	bool flag = false; // 记录是否有两个相邻元素交换顺序
+	for (i = 1; i < len; ++i) // len-1趟
+	{
+		for (j = 0; j < len - i; ++j)
+			if (arr[j] > arr[j + 1])
+			{
+				swap(arr[j], arr[j + 1]);
+				flag = true;
+			}
+			else flag = false; // 相邻元素的位置都正确，则整个序列都是排序的
+		if (!flag) return;
+
+	}
+}
+
+// 选择排序
+template<typename T>
+void select_sort(T *arr, int len)
+{
+	if (!arr || len <= 0) return;
+	int i, j;
+	for (i = 0; i < len - 1; ++i)
+		for (j = i + 1; j < len; ++j)
+			if (arr[i] > arr[j]) swap(arr[i], arr[j]);
+}
+
 };
 
 template <typename T>
@@ -53,26 +120,38 @@ public:
 	}
 };
 
-int main()
-{
-	vector<int> a;
-	a.push_back(3);
-	a.push_back(2);
-	a.push_back(8);
-	a.push_back(6);
-	a.push_back(9);
-	a.push_back(7);
-	a.push_back(4);
-	autumnlight::quick_sort_recursion(a.data(), a.size());
+template <typename T>
+class Print {
+public:
+	void operator() (T &t, string delimiter = " ") {
+		cout << t << delimiter;
+	}
+};
 
-	//for_each(a.begin(), a.end(), Print<int>());
-	int b[] = {1, 2, 1, 2, 1, 2};
-	int len = sizeof(b) / sizeof(int);
-	autumnlight::partition(b, len, IsOne<int>());
-	for (int i = 0; i < len; ++i)
-		cout << b[i] << " ";
-	cout << endl;
-	system("PAUSE");
-	return 0;
-}
+//int main()
+//{
+//	vector<int> a;
+//	a.push_back(3);
+//	a.push_back(2);
+//	a.push_back(8);
+//	a.push_back(6);
+//	a.push_back(9);
+//	a.push_back(7);
+//	a.push_back(4);
+//
+//	//autumnlight::quick_sort_nonrecursion(a.data(), a.size());
+//	//autumnlight::insert_sort(a.data(), a.size());
+//	autumnlight::bubble_sort(a.data(), a.size());
+//
+//	for_each(a.begin(), a.end(), Print<int>());
+//	cout << endl;
+//
+//	int b[] = {2, 1, 2, 1, 2, 1};
+//	int len = sizeof(b) / sizeof(int);
+//	autumnlight::partition2(b, len, IsOne<int>());
+//	for_each(begin(b), end(b), Print<int>());
+//	cout << endl;
+//	system("PAUSE");
+//	return 0;
+//}
 
